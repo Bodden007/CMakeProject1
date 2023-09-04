2800c57eaf0abb2d548d1218c34e3560ef06bc3c
Bodden007 <denis-bodrov@mail.ru>
2023-09-04 15:08:01 +0500

    Добавьте файлы проекта.

---
+++ src/CMakeProject1.cpp
@@ -0,0 +1,13 @@
+﻿// CMakeProject1.cpp: определяет точку входа для приложения.
+//
+#include <iostream>
+
+//#include "CMakeProject1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello ЧУВАК два РАЗА" << endl;
+	return 0;
+}
